91df89f3e1ed489dfcf21df0a47e0a4d5f224cf2
Joona Piirainen <joona.piirainen@gmail.com>
2020-08-09 15:49:43 +0300

    apollo link error fix

--- src/graphql/Client.tsx
+++ src/graphql/Client.tsx
@@ -1,9 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { createHttpLink } from 'apollo-link-http'
 
-const link = createHttpLink({
-  uri: 'https://mpjk0plp9.lp.gql.zone/graphql',
-})
+const link: any = createHttpLink({ uri: '/graphql' })
 
 export const client = new ApolloClient({
   link,
